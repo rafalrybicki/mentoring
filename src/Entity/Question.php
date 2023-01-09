bd116cc5f39069f1aeec63890559e4f9c3075993
Rafał Rybicki <rafarybicki@gmail.com>
2023-01-09 16:50:09 +0100

    Add question entity

---
+++ src/Entity/Question.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QuestionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: QuestionRepository::class)]
+class Question
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $content = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $answers = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getAnswers(): ?string
+    {
+        return $this->answers;
+    }
+
+    public function setAnswers(string $answers): self
+    {
+        $this->answers = $answers;
+
+        return $this;
+    }
+}
