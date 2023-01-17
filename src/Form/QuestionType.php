3518a8609cd5354dac4f03ea99dbf24d9dfcafaa
Rafał Rybicki <rafarybicki@gmail.com>
2023-01-17 20:31:22 +0100

    Change question form

--- src/Form/QuestionType.php
+++ src/Form/QuestionType.php
@@ -6,6 +6,7 @@ use App\Entity\Question;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +15,7 @@ class QuestionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('content')
+            ->add('content', TextType::class)
             ->add('answers', CollectionType::class, [
                 'entry_type' => AnswerType::class,
                 'entry_options' => ['label' => false],
