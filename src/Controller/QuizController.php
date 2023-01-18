35917170a0f1565f341ef3693f9591b59ca455ac
Rafał Rybicki <rafarybicki@gmail.com>
2023-01-18 18:59:40 +0100

    Add quiz controller

---
+++ src/Controller/QuizController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Quiz;
+use App\Form\QuizType;
+use App\Repository\QuizRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/quizes')]
+class QuizController extends AbstractController
+{
+    public QuizRepository $quizRepository;
+
+    public function __construct(QuizRepository $quizRepository)
+    {
+        $this->quizRepository = $quizRepository;
+    }
+
+    #[Route('/', name: 'app_quiz_index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('quiz/index.html.twig', [
+            'quizzes' => $this->quizRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_quiz_new', methods: ['GET', 'POST'])]
+    public function new(Request $request): Response
+    {
+        $quiz = new Quiz();
+        $form = $this->createForm(QuizType::class, $quiz);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->quizRepository->save($quiz, true);
+
+            return $this->redirectToRoute('app_quiz_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('quiz/new.html.twig', [
+            'quiz' => $quiz,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_quiz_show', methods: ['GET'])]
+    public function show(Quiz $quiz): Response
+    {
+        return $this->render('quiz/show.html.twig', [
+            'quiz' => $quiz,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_quiz_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Quiz $quiz): Response
+    {
+        $form = $this->createForm(QuizType::class, $quiz);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->quizRepository->save($quiz, true);
+
+            return $this->redirectToRoute('app_quiz_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('quiz/edit.html.twig', [
+            'quiz' => $quiz,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_quiz_delete', methods: ['POST'])]
+    public function delete(Request $request, Quiz $quiz): Response
+    {
+        if (!$this->isCsrfTokenValid('quiz_delete' . $quiz->getId(), $request->request->get('_token'))) {
+            throw $this->createAccessDeniedException("Incorrect token");
+        }
+
+        $this->quizRepository->remove($quiz, true);
+
+        return $this->redirectToRoute('app_quiz_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
